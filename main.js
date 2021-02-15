f820bb99dfb14eeeb09c75e3d59f6cfc7d6cb6ac
unikittyTS5 <76981446+unikittyTS5@users.noreply.github.com>
2021-02-14 17:44:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+name_of_student_array = [];
+function submit()
+{
+
+    var name_1 = document.getElementById ("student1").value;
+    var name_2 = document.getElementById ("student2").value;
+    var name_3 = document.getElementById ("student3").value;
+    var name_4 = document.getElementById ("student4").value;
+    name_of_student_array.push(name_1);
+
+    name_of_student_array.push(name_2);
+
+    name_of_student_array.push(name_3);
+
+    name_of_student_array.push(name_4);
+
+    console.log (name_of_student_array);
+
+    document.getElementById("display_name").innerHTML = name_of_student_array
+    document.getElementById("submit_button").style.display = "none"
+    document.getElementById("sort_button").style.display = "inline-block";
+}
+
+function sorting()
+{
+    name_of_student_array.sort();
+    console.log(name_of_student_array);
+    document.getElementById("display_name").innerHTML = name_of_student_array;
+}
